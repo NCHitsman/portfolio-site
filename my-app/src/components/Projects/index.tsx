f7d80e65066157c268da7ee2f426bcbb307759b6
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-08-01 16:50:44 -0700

    adding structure to back of cards

--- my-app/src/components/Projects/index.tsx
+++ my-app/src/components/Projects/index.tsx
@@ -10,10 +10,10 @@ interface props {
 }
 
 const Projects = ({ projects, setProjects, setAbout, setLastPage }: props) => {
-    const [flipped1, setFlipped1] = useState<boolean>(false)
-    const [flipped2, setFlipped2] = useState<boolean>(false)
-    const [flipped3, setFlipped3] = useState<boolean>(false)
-    const [flipped4, setFlipped4] = useState<boolean>(false)
+    const [flipped1, setFlipped1] = useState<boolean>(false);
+    const [flipped2, setFlipped2] = useState<boolean>(false);
+    const [flipped3, setFlipped3] = useState<boolean>(false);
+    const [flipped4, setFlipped4] = useState<boolean>(false);
 
     return (
         <>
@@ -31,19 +31,65 @@ const Projects = ({ projects, setProjects, setAbout, setLastPage }: props) => {
                         <div className="ProjectsTitle">Projects</div>
                     </div>
                     <div className="ProjectsCardCont">
-                        <div onClick={() => flipped1 ? setFlipped1(false) : setFlipped1(true)} className={flipped1 ? "ProjectCard Flipped" : 'ProjectCard'}>
+                        <div
+                            onClick={() =>
+                                flipped1
+                                    ? setFlipped1(false)
+                                    : setFlipped1(true)
+                            }
+                            className={
+                                flipped1 ? "ProjectCard Flipped" : "ProjectCard"
+                            }
+                        >
+                            <div className="ProjectCardFace Front">Project</div>
+                            <div className="ProjectCardFace Back">
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            onClick={() =>
+                                flipped2
+                                    ? setFlipped2(false)
+                                    : setFlipped2(true)
+                            }
+                            className={
+                                flipped2 ? "ProjectCard Flipped" : "ProjectCard"
+                            }
+                        >
                             <div className="ProjectCardFace Front">Project</div>
                             <div className="ProjectCardFace Back">Back</div>
                         </div>
-                        <div onClick={() => flipped2 ? setFlipped2(false) : setFlipped2(true)} className={flipped2 ? "ProjectCard Flipped" : 'ProjectCard'}>
+                        <div
+                            onClick={() =>
+                                flipped3
+                                    ? setFlipped3(false)
+                                    : setFlipped3(true)
+                            }
+                            className={
+                                flipped3 ? "ProjectCard Flipped" : "ProjectCard"
+                            }
+                        >
                             <div className="ProjectCardFace Front">Project</div>
                             <div className="ProjectCardFace Back">Back</div>
                         </div>
-                        <div onClick={() => flipped3 ? setFlipped3(false) : setFlipped3(true)} className={flipped3 ? "ProjectCard Flipped" : 'ProjectCard'}>
-                            <div className="ProjectCardFace Front">Project</div>
-                            <div className="ProjectCardFace Back">Back</div>
-                        </div>
-                        <div onClick={() => flipped4 ? setFlipped4(false) : setFlipped4(true)} className={flipped4 ? "ProjectCard Flipped" : 'ProjectCard'}>
+                        <div
+                            onClick={() =>
+                                flipped4
+                                    ? setFlipped4(false)
+                                    : setFlipped4(true)
+                            }
+                            className={
+                                flipped4 ? "ProjectCard Flipped" : "ProjectCard"
+                            }
+                        >
                             <div className="ProjectCardFace Front">Project</div>
                             <div className="ProjectCardFace Back">Back</div>
                         </div>
